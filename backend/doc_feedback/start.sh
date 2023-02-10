94d92e5fb6ed66a17fd65296d3dd140cdb4d2bb0
6r1d <vic.6r1d@gmail.com>
2023-02-10 04:21:58 +0200

    Store the newly generated secret
    Signed-off-by: 6r1d <vic.6r1d@gmail.com>

--- backend/doc_feedback/start.sh
+++ backend/doc_feedback/start.sh
@@ -7,6 +7,10 @@ AROOT="/home/fbc/doc_feedback"
 IROOT="$AROOT/installation"
 # Define the installation marker filename
 INSTALLED="$AROOT/ready"
+# Check that sectet key is available
+if [ ! -f "$INSTALLED" ]; then
+    echo "export SECRET_KEY='$(openssl rand -hex 40)'" > "$AROOT/.DJANGO_SECRET_KEY"
+fi
 # Load Django secrets
 source "$AROOT/.DJANGO_SECRET_KEY"
 # Update PATH
